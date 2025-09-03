df022017e9bfae6b317fff9b956e8a94506949e1
Sumit Maurya <mauryasumit222@gmail.com>
2025-09-03 22:46:59 +0530

    Add queue db config for

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "home#index"
 end
